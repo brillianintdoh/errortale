922daaaa3b578373f3e8aaec89fb47e11c92c431
brillianintdoh <palanghwi@gmail.com>
2024-05-04 09:30:50 +0900

    item

--- src/part_2/mainNode/part2.cpp
+++ src/part_2/mainNode/part2.cpp
@@ -13,6 +13,7 @@ Part2::Part2() {
     sleep_s = 0;
     sleep_b = 0;
     isShake = 1;
+    isBlack = 0;
     json_index = 0;
 }
 
@@ -41,7 +42,7 @@ void Part2::shake(double delta) {
 void Part2::black_scene(double delta) {
     sleep_b+=delta;
 
-    if(wait[1] >= sleep_s) {
+    if(wait[1] >= sleep_b) {
         background->set_z_index(5);
         sleep_b = 0;
     }else {
@@ -51,29 +52,65 @@ void Part2::black_scene(double delta) {
 
 void Part2::_ready() {
     user = Object::cast_to<CharacterBody2D>(get_node_internal("Heart"));
+    sans = Object::cast_to<AnimatedSprite2D>(get_node_internal("sans"));
     camera = Object::cast_to<Camera2D>(get_node_internal("Camera"));
     background = Object::cast_to<TextureRect>(get_node_internal("background"));
     sound = Object::cast_to<AudioStreamPlayer>(get_node_internal("sound"));
+    ui = Object::cast_to<Node2D>(get_node_internal("UI"));
+    tools_menu = Object::cast_to<Control>(get_node_internal("UI/menu/tools"));
+    tools[0] = Object::cast_to<RichTextLabel>(get_node_internal("UI/menu/tools/tools_1"));
     Ref<FileAccess> data_file = FileAccess::open("res://data/battle/battle.json", FileAccess::READ);
     Ref<FileAccess> tools_file = FileAccess::open("res://data/battle/tool.json", FileAccess::READ);
     data_json = json->parse_string(data_file->get_as_text());
     tools_json = json->parse_string(tools_file->get_as_text());
 
-    Callable fun = callable_mp(this, &Part2::sound_end);
-    sound->connect("finished", fun);
+    Callable fun_sound = callable_mp(this, &Part2::sound_end);
+    Callable fun_sans = callable_mp(this, &Part2::sansAnim_end);
+    sound->connect("finished", fun_sound);
+    sans->connect("animation_finished", fun_sans);
     sound->play();
+
+
+    // 아이템 메뉴에 추가
+    int x = 45,y = 25, p = 1;
+    tools[0]->set_position(Vector2(x,y));
+    tools[0]->set_text(tools_json.get(0).get("item"));
+    for(int i=1; i < 8; i++) {
+        if(p) { 
+            y+=75;
+        }else {
+            y-=75;
+            x+=200;
+        }
+        tools[i] = (RichTextLabel*) tools[0]->duplicate();
+        tools[i]->set_position(Vector2(x,y));
+        tools[i]->set_text(tools_json.get(i).get("item"));
+        tools_menu->add_child(tools[i]);
+        p = (p == 1) ? 0 : 1;
+    }
 }
 
 void Part2::sound_end() {
-    // sound->get_stream()->set_path(data_json.get(json_index).get("sound"));
-    // sound->play();
-    // json_index+=1;
-    // isShake = 0;
+    sound->get_stream()->set_path(data_json.get(json_index).get("sound"));
+    json_index+=1;
+    isShake = 0;
+    isBlack = 1;
+    camera->set_drag_horizontal_offset(0);
+    camera->set_drag_vertical_offset(0);
+    sans->play("sans_start");
+    sound->play();
+}
+
+void Part2::sansAnim_end() {
+    printf("wow");
 }
 
 void Part2::_process(double delta) {
     if(isShake) {
-        // shake(delta);
-        // black_scene(delta);
+        shake(delta);
+    }
+
+    if(isBlack) {
+        black_scene(delta);
     }
 }
--- src/part_2/mainNode/part2.h
+++ src/part_2/mainNode/part2.h
@@ -2,12 +2,15 @@
 #define Part2_H
 #include "../../main/env/main.h"
 #include<godot_cpp/classes/node2d.hpp>
+#include<godot_cpp/classes/rich_text_label.hpp>
+#include<godot_cpp/classes/control.hpp>
 #include<godot_cpp/classes/texture_rect.hpp>
 #include<godot_cpp/classes/resource_loader.hpp>
 #include<godot_cpp/classes/json.hpp>
 #include<godot_cpp/variant/variant.hpp>
 #include<godot_cpp/classes/audio_stream.hpp>
 #include<godot_cpp/classes/audio_stream_player.hpp>
+#include<godot_cpp/classes/animated_sprite2d.hpp>
 namespace godot {
     class Part2 : public Node2D {
         GDCLASS(Part2, Node2D);
@@ -21,20 +24,25 @@ namespace godot {
             void shake(double delta);
             void black_scene(double delta);
             void sound_end();
+            void sansAnim_end();
 
             void _ready() override;
             void _process(double delta) override;
         
         private:
-            int isShake, json_index;
+            int isShake, isBlack, json_index;
             double wait[4];
             double sleep_s, sleep_b;
             ResourceLoader *res;
             JSON *json;
             Variant data_json;
             Variant tools_json;
+            Control* tools_menu;
+            RichTextLabel* tools[8];
             TextureRect* background;
             AudioStreamPlayer* sound;
+            AnimatedSprite2D* sans;
+            Node2D* ui;
     };
 }
 #endif
